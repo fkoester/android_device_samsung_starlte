bradc6/android_device_samsung_starlte-1

starlte: Consider lineage_star2lte product via the AOSP way

AndroidProducts.mk
@@ -1,0 +1,5 @@
+# Copyright (C) 2018 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_starlte.mk

fkoester/android_device_samsung_starlte

AndroidProducts.mk
@@ -1,0 +1,5 @@
+# Copyright (C) 2018 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage.mk